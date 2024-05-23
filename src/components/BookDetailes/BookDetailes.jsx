5eca531e2e4c62f42eb5cc4220f4081c3fda2e55
jakariacst318 <jsjakariacst@gmail.com>
2024-05-23 14:11:15 +0600

    view details all data

--- src/components/BookDetailes/BookDetailes.jsx
+++ src/components/BookDetailes/BookDetailes.jsx
@@ -5,17 +5,34 @@ const BookDetailes = () => {
     const { id } = useParams()
     const idInt = parseInt(id)
     const book = books.find(book => book.id === idInt)
-    const {image,review } = book
+    const { image, review, bookName, author, category, tags, totalPages, publisher, yearOfPublishing, rating } = book
     return (
         <div>
-            <h2 className="text-3xl"> BookDetailes</h2>
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row">
+                <div className="hero-content flex-col lg:flex-row gap-x-20">
                     <img src={image} className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">{review}</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <h1 className="text-5xl font-bold pb-5">{bookName}</h1>
+                        <p className="text-[#131313CC] font-medium text-xl"> By: {author}</p>
+                        <p className="text-[#131313CC] font-medium text-xl py-10">{category}</p>
+                        <p className="text-[#131313] font-medium pb-6">Review : <span className="text-[#131313B3]">{review}</span></p>
+                        <div className="flex gap-x-9 font-medium text-[#23BE0A] pb-12">
+                            <h2 className="text-[#131313CC]">Tag </h2>
+                            {
+                                tags.map((ing, idx) => <p key={idx}>{ing}</p>)
+                            }
+                        </div>
+                        <div className="space-y-3 ">
+                            <p className="text-[#131313B3]">pages <span className="font-semibold text-[#131313] ms-[140px]">{totalPages}</span></p>
+
+                            <p className="text-[#131313B3]">Publisher <span className="font-semibold text-[#131313] ms-[115px]">{publisher}</span></p>
+
+                            <p className="text-[#131313B3]">Year Of Publishing <span className="font-semibold text-[#131313] ms-12">{yearOfPublishing}</span></p>
+
+                            <p className="text-[#131313B3] pb-8">Rating <span className="font-semibold text-[#131313] ms-[130px]">{rating}</span></p>
+                        </div>
+                        <button className="btn btn-outline btn-accent">Read</button> 
+                        <button className="btn btn-accent ms-4">Wishlist</button>
                     </div>
                 </div>
             </div>
